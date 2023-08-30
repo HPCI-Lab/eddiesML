e38b5b8823375b29523e4d25835e75e9f1da9e00
massimilianofronza <maxfronza97@gmail.com>
2023-08-30 19:38:04 +0200

    Add edge_index for PyG dataset

--- src/Dataset.py
+++ src/Dataset.py
@@ -8,7 +8,8 @@ import xarray as xr
 class PilotDataset(Dataset):
 
     # root: Where the dataset should be stored and divided into processed/ and raw/
-    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+    def __init__(self, root, mesh_path, transform=None, pre_transform=None, pre_filter=None):
+        self.mesh_path = mesh_path
         super().__init__(root, transform, pre_transform, pre_filter)
 
     @property
@@ -29,40 +30,39 @@ class PilotDataset(Dataset):
     # This function is triggered as soon as the PilotDataset is instantiated
     def process(self):
         
-        # Conserving adjacency info to avoid computing it every time
+        # Get the adjacency info(common for all our graphs)
+        edge_index = self._get_adjacency_info()
+        
+        node_feats = None
         edge_index = None
-
+        labels = None
+        
         for raw_path in self.raw_paths:
-
-            # TODO for each file I was extracting year and cyclone number. Here we'll have something else
-            year = raw_path.split('_')[2]
-            cyclone = raw_path.split('_')[4].split('.')[0]
-            #print(f'    Year {year}, Patch number {cyclone}...')
+            file_name = raw_path.split('/')[-1]
+            year = file_name.split('_')[2]
+            month = file_name.split('_')[3]
+            day = file_name.split('_')[4].split('.')[0]
+            
+            #print(f'    Year {year}, Month {month}, Day {day}...')
+            
             raw_data = xr.open_dataset(raw_path)
 
             # Get node features
-            node_feats = self._get_node_features(raw_data)
-
-            # Get edge features - TODO do we need to assign here the physical distance between nodes?
-            #edge_feats = self._get_edge_features(raw_data)
-
-            # Get adjacency info
-            if edge_index==None:
-                edge_index = self._get_adjacency_info(raw_data)
-
+            #node_feats = self._get_node_features(raw_data)
+            
             # Get labels info
-            labels = self._get_labels(raw_data)
+            #labels = self._get_labels(raw_data)
 
             # Create the Data object
             data = Data(
-                x=node_feats,                       # node features
+                #x=node_feats,                       # node features
                 edge_index=edge_index,              # edge connectivity
-                y=labels,                           # labels for classification
+                #y=labels,                           # labels for classification
             )
 
             #print(os.path.join(self.processed_dir, f'year_{year}_cyclone_{cyclone}.pt'))
             # TODO customize this to fit the type of eddy information we're going to store
-            torch.save(data, os.path.join(self.processed_dir, f'year_{year}_cyclone_{cyclone}.pt'))
+            torch.save(data, os.path.join(self.processed_dir, f'year_{year}_month_{month}_day_{day}.pt'))
 
         print("    Shape of node feature matrix:", np.shape(node_feats))
         print("    Shape of graph connectivity in COO format:", np.shape(edge_index))
@@ -80,23 +80,11 @@ class PilotDataset(Dataset):
         all_nodes_feats = np.asarray(all_nodes_feats)
         return torch.tensor(all_nodes_feats, dtype=torch.float)
 
-
-    # Retrieve the edge features
-    def _get_edge_features(self, data):
-        all_edge_feats = []
-        all_edge_feats = np.asarray(all_edge_feats)
-        return all_edge_feats
-
-
+    # TODO it should be undirected, see utils.to_undirected() to make it undirected
     # Return the graph connetivity in COO format with shape=[2, num_edges]
-    def _get_adjacency_info(self, data):
-        
-        coo_links = [[], []]
-
-        # TODO for every edge, append start and end node in coo_links[0] and coo_links[1]
-
-        return torch.tensor(coo_links, dtype=torch.long)
-
+    def _get_adjacency_info(self):        
+        mesh = xr.open_dataset(self.mesh_path)
+        return torch.tensor(mesh.edges.values.T, dtype=torch.long)
 
     # Download the raw data into raw/, or the folder specified in self.raw_dir
     def download(self):
@@ -107,6 +95,6 @@ class PilotDataset(Dataset):
         return len(self.processed_file_names)
     
     # Implements the logic to load a single graph - TODO we'll have to redefine this
-    def get(self, year, cyclone):
-        data = torch.load(os.path.join(self.processed_dir, f'year_{year}_cyclone_{cyclone}.pt'))
+    def get(self, year, month, day):
+        data = torch.load(os.path.join(self.processed_dir, f'year_{year}_month_{month}_day_{day}.pt'))
         return data
