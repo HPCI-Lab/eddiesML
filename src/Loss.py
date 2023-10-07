e7d4bc1dfe4627e285fd0e69087b63ddce29e6ee
massimilianofronza <maxfronza97@gmail.com>
2023-10-07 12:03:45 +0200

    Fix loss loading at every forward()

--- src/Loss.py
+++ src/Loss.py
@@ -1,12 +1,12 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
 class WeightedCrossEntropyLoss(nn.Module):
     def __init__(self, class_weights):
         super(WeightedCrossEntropyLoss, self).__init__()
-        self.class_weights = torch.tensor(class_weights, dtype=torch.float32)
+        class_weights = torch.tensor(class_weights, dtype=torch.float32)
+        self.ce_loss = nn.CrossEntropyLoss(weight=class_weights)
 
     def forward(self, logits, targets):
-        ce_loss = nn.CrossEntropyLoss(weight=self.class_weights)
-        return ce_loss(logits, targets)
-
+        return self.ce_loss(logits, targets)
