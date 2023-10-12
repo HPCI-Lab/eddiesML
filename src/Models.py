a80619a04115ca644e90d8e1e1a0bb799d90788d
Desta <dhesta26@gmail.com>
2023-10-12 12:32:42 +0200

    UNet and GCN model integration

--- src/Models.py
+++ src/Models.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from torch_geometric.nn import GraphUNet
@@ -38,3 +39,32 @@ class GUNet(torch.nn.Module):
         middle = self.act_middle.__name__
         final = self.act_final.__name__
         print(f"GUNet instantiated!\n\tMiddle act: {middle}\n\tFinal act: {final}")
+# A GCN model created for comparison purposes with the Graph U-Net model
+class GCNModel(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_nodes, num_layers):
+        super(GCNModel, self).__init__()
+        self.num_layers = num_layers
+        self.conv_layers = nn.ModuleList()
+        
+        # Add the input layer
+        self.conv_layers.append(GCNConv(in_channels, hidden_channels))
+        
+        # Add the hidden layers
+        for _ in range(num_layers - 2):  # -2 because we already added input and will add output later
+            self.conv_layers.append(GCNConv(hidden_channels, hidden_channels))
+        
+        # Add the output layer
+        self.conv_layers.append(GCNConv(hidden_channels, out_channels))
+        
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        for layer in self.conv_layers:
+            x = F.relu(layer(x, edge_index))
+        return F.log_softmax(x, dim=1)
+
+    def _log_network(self):
+        middle = nn.Relu()
+        final = nn.Softmax()
+        print(f"GCN instantiated!\n\tMiddle act: {middle}")#\n\tFinal act: {final}")
+        
+        
