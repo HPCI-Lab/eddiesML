d80545d98562acbe7fc52f8aac3d067ef43bc78d
Desta <dhesta26@gmail.com>
2023-11-16 11:30:23 +0100

    Focal loss and ensemble models

--- src/Loss.py
+++ src/Loss.py
@@ -12,28 +12,6 @@ class WeightedCrossEntropyLoss(nn.Module):
         return ce_loss(logits, targets)
     
 class SoftDiceLoss(nn.Module):
-    def __init__(self, smooth=1):
-        super(SoftDiceLoss, self).__init__()
-        self.smooth = smooth
-
-    def forward(self, logits, labels):
-        num_classes = logits.size(1)
-        probs = torch.softmax(logits, dim=1)
-        dice_scores = torch.zeros(num_classes, dtype=logits.dtype, device=logits.device)
-
-        for class_idx in range(num_classes):
-            class_probs = probs[:, class_idx, ...]
-            class_labels = (labels == class_idx).float()
-
-            intersection = (class_probs * class_labels).sum()
-            union = class_probs.sum() + class_labels.sum()
-
-            dice_scores[class_idx] = (2.0 * intersection + self.smooth) / (union + self.smooth)
-
-        loss = 1.0 - dice_scores.mean()
-        return loss
-    
-class WiightedSoftDiceLoss(nn.Module):
     def __init__(self, class_weights, smooth=1):
         super(SoftDiceLoss, self).__init__()
         
@@ -56,3 +34,39 @@ class WiightedSoftDiceLoss(nn.Module):
         weighted_mean = (self.class_weights[0]*dice_scores[0] + self.class_weights[1]*dice_scores[1] + self.class_weights[2]*dice_scores[2])
         loss = 1.0 - weighted_mean
         return loss
+
+# Focal loss
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2, alpha=None, class_weights=[0.0238, 0.5028, 0.4734], reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        self.class_weights = class_weights
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        num_classes = input.size(1)
+        probs = F.softmax(input, dim=1)
+        one_hot = F.one_hot(target, num_classes=input.size(1)).float()
+        pt = (probs * one_hot).sum(1) + 1e-9
+        focal_loss = -((1 - pt).pow(self.gamma)) * torch.log(pt)
+        
+        if self.alpha is not None:
+            alpha_factor = one_hot * self.alpha + (1 - one_hot) * (1 - self.alpha)
+            focal_loss = focal_loss * alpha_factor
+
+        if self.class_weights is not None:
+            class_weights = torch.tensor(self.class_weights, dtype=torch.float, device=input.device)
+            # Expand class_weights to match the size of focal_loss
+            #class_weights = class_weights.view(1, -1)
+            class_weights = (self.class_weights[0]*focal_loss[0] + self.class_weights[1]*focal_loss[1] + self.class_weights[2]*focal_loss[2])
+            focal_loss = focal_loss * class_weights
+
+        if self.reduction == 'mean':
+            return focal_loss.mean()
+        elif self.reduction == 'sum':
+            return focal_loss.sum()
+        elif self.reduction == 'none':
+            return focal_loss
+        else:
+            raise ValueError("Invalid reduction option. Use 'mean', 'sum', or 'none'.")
--- src/Models.py
+++ src/Models.py
@@ -5,13 +5,13 @@ from torch_geometric.nn import GCNConv
 from torch_geometric.nn import GraphUNet
 from torch_geometric.nn import SAGEConv
 from torch_geometric.utils import dropout_edge
-
+import Loss
 
 # Graph U-Net class
 class GUNet(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, num_nodes, final_act):
         super().__init__()
-
+        
         self.act_middle = torch.nn.functional.relu
         self.act_final = final_act
         
@@ -42,73 +42,110 @@ class GUNet(torch.nn.Module):
     
 # A GCN model created for comparison purposes with the Graph U-Net model
 class GCNModel(nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_nodes, num_layers):
+    def __init__(self, num_features, hidden_dim, num_classes, num_layers, num_nodes, final_act):
         super(GCNModel, self).__init__()
+        
         self.num_layers = num_layers
-        self.conv_layers = nn.ModuleList()
+        self.act_middle = torch.nn.functional.relu
+        self.act_final = final_act
         
-        # Add the input layer
-        self.conv_layers.append(GCNConv(in_channels, hidden_channels))
-        
-        # Add the hidden layers
-        for _ in range(num_layers - 2):  # -2 because we already added input and will add output later
-            self.conv_layers.append(GCNConv(hidden_channels, hidden_channels))
-        
-        # Add the output layer
-        self.conv_layers.append(GCNConv(hidden_channels, out_channels))
-        
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-        for layer in self.conv_layers:
-            x = F.relu(layer(x, edge_index))
-        return F.log_softmax(x, dim=1)
-
-    def _log_network(self):
-        middle = nn.Relu()
-        final = nn.Softmax()
-        print(f"GCN instantiated!\n\tMiddle act: {middle}")#\n\tFinal act: {final}")
-        
-# Define the GraphSAGE model class
-class GraphSAGEModel(nn.Module):
-    def __init__(self, num_features, hidden_dim, num_classes, num_layers, normalize=True, dropout=0.0):
-        super(GraphSAGEModel, self).__init__()
-
-        self.num_layers = num_layers
+        pool_ratios = [2000 / num_nodes, 0.5]
 
         # List to store the GraphSAGE convolutional layers
         self.conv_layers = nn.ModuleList()
 
         # Input layer
-        self.conv_layers.append(SAGEConv(num_features, hidden_dim))
+        self.conv_layers.append(GCNConv(num_features, hidden_dim, pool_ratios=pool_ratios, act=self.act_middle))
 
         # Apply He initialization to the underlying linear transformations
-        for i in range(len(self.conv_layers)):
-            nn.init.kaiming_uniform_(self.conv_layers[i].lin_l.weight)
-            nn.init.kaiming_uniform_(self.conv_layers[i].lin_r.weight)
+        # for i in range(len(self.conv_layers)):
+        #     nn.init.kaiming_uniform_(self.conv_layers[i].lin_l.weight)
+        #     nn.init.kaiming_uniform_(self.conv_layers[i].lin_r.weight)
 
         # Intermediate layers
         for _ in range(1, num_layers - 1):
-            self.conv_layers.append(SAGEConv(hidden_dim, hidden_dim))
+            self.conv_layers.append(GCNConv(hidden_dim, hidden_dim, pool_ratios=pool_ratios, act=self.act_middle))
 
         # Output layer
-        self.conv_layers.append(SAGEConv(hidden_dim, num_classes))
+        self.conv_layers.append(GCNConv(hidden_dim, num_classes, pool_ratios=pool_ratios, act=self.act_final))
 
-        self.normalize = normalize
+        #self.normalize = normalize
 
+        
+        
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+        edge_index, _ = dropout_edge(data.edge_index, p=0.2,
+                                     force_undirected=True,
+                                     training=self.training)
+        x = F.dropout(data.x, p=0.92, training=self.training)
+        for layer in self.conv_layers:
+            x = layer(x, edge_index)
 
-        for i in range(self.num_layers):
-            x = self.conv_layers[i](x, edge_index)
+            act = None
+            if self.act_final.__name__ == "softmax":
+                act = self.act_final(x, dim=1)
+            elif self.act_final.__name__ == "sigmoid":
+                act = self.act_final(x)
+        return act
 
-            if i < self.num_layers - 1:
-                x = F.relu(x)
+    def _log_network(self):
+        middle = self.act_middle.__name__
+        final = self.act_final.__name__
+        print(f"GCN instantiated!\n\tMiddle act: {middle}\n\tFinal act: {final}")   
+        
+# Define the GraphSAGE model class
+class GraphSAGEModel(nn.Module): 
+    def __init__(self, num_features, hidden_dim, num_classes, num_layers, num_nodes, final_act):#, normalize=True, dropout=0.0):
+        super(GraphSAGEModel, self).__init__()
 
-        if self.normalize:
-            #x = F.normalize(x, p=2, dim=-1)  # L2 normalization
-            x = x / (x.abs().sum(dim=1, keepdim=True) + 1e-8)
+        self.num_layers = num_layers
+        self.act_middle = torch.nn.functional.relu
+        self.act_final = final_act
+        
+        pool_ratios = [2000 / num_nodes, 0.5]
 
-        return F.log_softmax(x, dim=1)
+        # List to store the GraphSAGE convolutional layers
+        self.conv_layers = nn.ModuleList()
+
+        # Input layer
+        self.conv_layers.append(SAGEConv(num_features, hidden_dim, pool_ratios=pool_ratios, act=self.act_middle))
+
+        # Apply He initialization to the underlying linear transformations
+        # for i in range(len(self.conv_layers)):
+        #     nn.init.kaiming_uniform_(self.conv_layers[i].lin_l.weight)
+        #     nn.init.kaiming_uniform_(self.conv_layers[i].lin_r.weight)
+
+        # Intermediate layers
+        for _ in range(1, num_layers - 1):
+            self.conv_layers.append(SAGEConv(hidden_dim, hidden_dim, pool_ratios=pool_ratios, act=self.act_middle))
+
+        # Output layer
+        self.conv_layers.append(SAGEConv(hidden_dim, num_classes, pool_ratios=pool_ratios, act=self.act_final))
+
+        #self.normalize = normalize
+
+        
+        
+    def forward(self, data):
+        edge_index, _ = dropout_edge(data.edge_index, p=0.2,
+                                     force_undirected=True,
+                                     training=self.training)
+        x = F.dropout(data.x, p=0.92, training=self.training)
+        for layer in self.conv_layers:
+            x = layer(x, edge_index)
+
+            act = None
+            if self.act_final.__name__ == "softmax":
+                act = self.act_final(x, dim=1)
+            elif self.act_final.__name__ == "sigmoid":
+                act = self.act_final(x)
+        return act
+
+    def _log_network(self):
+        middle = self.act_middle.__name__
+        final = self.act_final.__name__
+        print(f"Graph Sage instantiated!\n\tMiddle act: {middle}\n\tFinal act: {final}")    
+        
     
     ## Possible improvements points
     # Number of layer might significantly impact its performance
@@ -116,3 +153,73 @@ class GraphSAGEModel(nn.Module):
     # Include or exclude L2 normalization - try
     # Number of Aggregation Neighbors (num_samples) - accuracy vs performance
     #Batch size, epoch, optimizer loss
+    
+# Definition of an Ensemble class to combine predictions of the three models
+class EnsembleModel(nn.Module):
+    def __init__(self, GUNet, GCNModel, GraphSAGEModel, weights=None):
+        super(EnsembleModel, self).__init__()
+        self.GUNet = GUNet
+        self.GCNModel = GCNModel
+        self.GraphSAGEModel = GraphSAGEModel
+
+        # Define weights for each model in the ensemble
+        if weights is None:
+            self.weights = nn.Parameter(torch.ones(3))  # Default: equal weights
+        else:
+            self.weights = nn.Parameter(torch.Tensor(weights))
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        # Apply the Graph U-Net to the feature vectors
+        x_unet = self.GUNet(data)
+        # Apply the GCN to the graph and feature vectors
+        x_gcn = self.GCNModel(data)
+        # Apply the GraphSAGE to the graph and feature vectors
+        x_sage = self.GraphSAGEModel(data)
+        x = (self.weights[0] * x_unet + self.weights[1] * x_gcn + self.weights[2] * x_sage) / sum(self.weights)
+        return x
+        
+#Implementation of voting ensemble
+class VotingEnsemble(nn.Module):
+    def __init__(self, models):
+        super(VotingEnsemble, self).__init__()
+        self.models = nn.ModuleList(models)
+
+    def forward(self, data):
+        predictions = [model(data) for model in self.models]
+        # Soft voting: Combine predictions using element-wise averaging
+        ensemble_output = torch.stack(predictions, dim=0).mean(dim=0)
+        return ensemble_output
+    
+# Focal loss fusion ensemble. 
+#Apply focal loss to each model's predictions before fusion to down-weights the background class  and up-weight cyclonic and anticyclonic eddies classes to counter the imbalanced datasets.
+class EnsembleFocal(nn.Module):
+    def __init__(self, GUNet, GCNModel, GraphSAGEModel, weights=None, focal_loss_params=None):
+        super(EnsembleFocal, self).__init__()
+        self.GUNet = GUNet
+        self.GCNModel = GCNModel
+        self.GraphSAGEModel = GraphSAGEModel
+
+        if weights is None:
+            self.weights = nn.Parameter(torch.ones(3))
+        else:
+            self.weights = nn.Parameter(torch.Tensor(weights))
+
+        # Focal loss parameters
+        self.focal_loss_params = focal_loss_params
+        self.focal_loss = Loss.FocalLoss(**focal_loss_params) if focal_loss_params else None
+
+    def forward(self, data):
+        x_unet = self.GUNet(data)
+        x_gcn = self.GCNModel(data)
+        x_sage = self.GraphSAGEModel(data)
+
+        # Combine model outputs with weights
+        combined_output = (self.weights[0] * x_unet + self.weights[1] * x_gcn + self.weights[2] * x_sage) / sum(self.weights)
+
+        # Apply focal loss if specified
+        if self.focal_loss:
+            target = data.y  # Assuming the ground truth labels are stored in data.y
+            loss = self.focal_loss(combined_output, target)
+            return combined_output, loss
+        else:
+            return combined_output
